cf9b3d14f6e09550f5544b7b2873805082d84686
Cameron Dutro <camertron@gmail.com>
2017-04-04 16:29:17 -0700

    Initial commit

---
+++ spec/innuendo-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import InnuendoAtomView from '../lib/innuendo-atom-view';
+
+describe('InnuendoAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
